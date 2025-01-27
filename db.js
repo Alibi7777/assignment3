6d0a7140f7464d7878ee2d85fdf17c81f69fd4de
Alibi7777 <alibiospan604@gmail.com>
2025-01-27 22:38:46 +0500

    changed env

--- db.js
+++ db.js
@@ -4,7 +4,10 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         mongoose.set('strictQuery', false);
-        const conn = await mongoose.connect(process.env.MONGO_URI); 
+//MONGO_URI=mongodb+srv://alibi:1234@cluster0.gg1ix.mongodb.net/assignment3?retryWrites=true&w=majority
+
+        const conn = await mongoose.connect("mongodb+srv://alibi:1234@cluster0.gg1ix.mongodb.net/assignment3?retryWrites=true&w=majority"); 
+        // const conn = await mongoose.connect(process.env.MONGO_URI); 
         console.log(`Database connected: ${conn.connection.host}`);
     } catch (error) {
         console.error("Database connection error:", error);
